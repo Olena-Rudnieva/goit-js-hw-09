ad58fc96e52ca9ef8e54ad83ce4c4dbe2b309244
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-26 20:26:40 +0300

    Add js in first tast

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.body;
+let timerId = null;
 
+buttonStart.addEventListener('click', onButtonStart);
+buttonStop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+}
+
+function onButtonStop() {
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
