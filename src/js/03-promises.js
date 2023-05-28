ee394546d3d1eebd9b1f73d2ac1f2a1235ff1514
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-28 14:38:50 +0300

    Update js file

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,59 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const buttonEl = document.querySelector('.form button');
+const firstDelayEl = document.querySelector('input[name="delay"]');
+const delayStepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+
+firstDelayEl.addEventListener('input', onFirstDelay);
+amountEl.addEventListener('input', onAmountPromises);
+delayStepEl.addEventListener('input', onDelayStep);
+buttonEl.addEventListener('click', onCreatePromiseBtn);
+
+function onFirstDelay(value) {
+  console.log(firstDelayEl.value);
+}
+
+function onDelayStep() {
+  console.log(delayStepEl.value);
+}
+
+function onAmountPromises(number) {
+  console.log(amountEl.value);
+}
+
+function onCreatePromiseBtn() {
+  let amountNumber = amountEl.value;
+  let firstDelayOption = firstDelayEl.value;
+  let delayStepOption = delayStepEl.value;
+
+
+  for (let i = 1; i <= amountNumber; i += 1) {
+    createPromise(i, firstDelayOption*i, delayStepOption)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      })  
+      .finally(() => {
+        console.log('тест');
+      
+      
+    })
+    
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
